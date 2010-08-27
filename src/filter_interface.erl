b6977dbef6910b9989c5e3f936c8fbd83bb3ed32
Boris Okner <b.okner@rogers.com>
2010-08-26 21:27:32 -0400

    Web interface for content filter: update notification

---
+++ src/filter_interface.erl
@@ -0,0 +1,48 @@
+%%-------------------------------------------------------------------
+%%% Copyright 2010 Cleartext
+%%% Author  : Boris Okner <boris.okner@gmail.com>
+%%% Description : Web interface to content filter
+%%%
+%%% Created: Aug 23, 2010
+
+-module(filter_interface).
+
+%%
+%% Include files
+%%
+-include("ejabberd_http.hrl").
+%%
+%% Exported Functions
+%%
+-export([process/2]).
+
+
+%%
+%% API Functions
+%%
+
+%%--------------------------------------------------------------------
+%%% Web interface
+%%--------------------------------------------------------------------
+
+process(["update"], #request{q = Query}) ->
+	Host = proplists:get_value("host", Query, undefined),
+	Result = case Host of 
+						 undefined ->
+							 "host is required";
+						 _ ->
+							 mod_content_filter:reload_rules(Host)
+					 end,
+   {xmlelement, "html", [{"xmlns", "http://www.w3.org/1999/xhtml"}],
+     [{xmlelement, "head", [],
+       [{xmlelement, "title", [], []}]},
+      {xmlelement, "body", [],
+       [{xmlelement, "p", [], [{xmlcdata, io_lib:format("~p", [Result])}]}]}]};
+
+process(_Other, _Request) ->
+	{400, [], {xmlelement, "h1", [],
+               [{xmlcdata, "400 Bad Request"}]}}.
+%%
+%% Local Functions
+%%
+
